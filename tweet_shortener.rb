9db990954a4f3d0282b598d478e205fd3dc8e04f
Aaron Chu <aaronchu357@gmail.com>
2019-04-08 22:11:08 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -2,7 +2,7 @@ def dictionary
   dictionary = {
     "hello" => 'hi',
     "two" => '2', "to" => '2', "too" => '2',
-    "four" => '4', "for" => '4', "For" => '4', "Four" => '4'
+    "four" => '4', "for" => '4', "For" => '4', "Four" => '4',
     'be' => 'b',
     'you' => 'u',
     "at" => "@",
