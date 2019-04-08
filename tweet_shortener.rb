2b105345bd557a17f97ee29c9eabd229aa7fd866
Aaron Chu <aaronchu357@gmail.com>
2019-04-08 22:30:01 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,35 @@
-# Write your code here.
+def dictionary
+  dictionary = {
+    "hello" => 'hi',
+    "two" => '2', "to" => '2', "too" => '2',
+    "four" => '4', "for" => '4', "For" => '4', "Four" => '4',
+    'be' => 'b',
+    'you' => 'u',
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+def word_substituter(str)
+  arr = str.split(" ")
+  arr.map! {|word| dictionary.keys.include?(word) ? dictionary[word] : word}
+  arr.join(" ")
+end
+
+def bulk_tweet_shortener(arr)
+  arr.each do |tweet|
+    puts word_substituter(tweet)
+  end
+end
+
+def selective_tweet_shortener(tweet)
+  tweet.length > 140 ? word_substituter(tweet) : tweet
+end
+
+def shortened_tweet_truncator(tweet)
+  shortend_tweet = tweet[0..139]
+  if tweet.length > 140
+    tweet[0..139] + "..."
+  end
+  shortend_tweet
+end
