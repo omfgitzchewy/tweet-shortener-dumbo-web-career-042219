b257ef7af7858c6783c33152bf3cf553665eb11a
Aaron Chu <aaronchu357@gmail.com>
2019-04-08 21:24:45 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+def dictionary
+  dictionary = {
+    "hello" => 'hi',
+    "two" => '2', "to" => '2', "too" => '2',
+    "four" => '4', "for" => '4',
+    'be' => 'b',
+    'you' => 'u',
+    "at" => "@",
+    "and" => "&"
+  }
+end
